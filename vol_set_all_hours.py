0ffa964fd4eafbe06c9132078995ed0884242e43
Kevin Saavedra <kevin.saavedra@oregonmetro.gov>
2018-07-10 08:22:45 -0700

    working New_Project creation behavior.

--- vol_set_all_hours.py
+++ vol_set_all_hours.py
@@ -6,19 +6,39 @@ Useage:
 """
 
 import os
+import sys
+import shutil
 import inro.emme.database.emmebank as _emmebank
 import inro.emme.desktop.app as _app
 import inro.modeller as _m
 
-def shapefile_export(scenario):
+def new_project(working_dir):
+    emmebank_path = os.path.join(working_dir, "emmebank")
+    try:    
+        shutil.rmtree(os.path.join(working_dir,"New_Project"))
+        project = _app.create_project(working_dir,"New_Project")#change to relative path
+    except WindowsError:
+        if os.path.exists(working_dir):
+            project = _app.create_project(working_dir,"New_Project")
+        else:
+            print "Path does not exist. Please enter an existing path."
+            sys.exit()        
+    
+    my_app = _app.start_dedicated(False, "000", project)
+    data = my_app.data_explorer()
+    bank = data.add_database(emmebank_path)
+    return my_app, bank
+
+def shapefile_export(working_dir, scenario, app):
     """Exports emme network as ArcGIS shapefile using standard modeller 
     toolbox."""
-    project_path = os.path.join(os.getcwd(), "New_Project/New_Project.emp")
-    export_path = os.path.join(os.getcwd(),  "New_Project/Media/Python_exported_scenario")
-    my_desktop = _app.start_dedicated(project = project_path, 
-                                      visible  = False, 
-                                      user_initials = "user")
-    my_modeller = _m.Modeller(my_desktop)
+    #emp_project_path = os.path.join(working_dir, "New_Project/New_Project.emp")
+    export_path = os.path.join(working_dir,  "New_Project/Media/Python_exported_scenario")
+    #my_desktop = _app.start_dedicated(project = emp_project_path, 
+    #                                  visible  = False, 
+    #                                  user_initials = "user")
+    #my_modeller = _m.Modeller(my_desktop)
+    my_modeller = _m.Modeller(app)
     network_2_shp = my_modeller.tool(
         "inro.emme.data.network.export_network_as_shapefile"
     )
@@ -30,9 +50,7 @@ def shapefile_export(scenario):
     }
     network_2_shp(export_path, False, 'SEGMENTS', scenario, selection)
 
-def main():
-    # TODO: try to refactor using a dictionary for inputs. 
-
+def attribute_copy():
     if '@am0708' not in _emmebank.Emmebank().scenario(2017).attributes('LINK'):
         _emmebank.Emmebank().scenario(2017).create_extra_attribute('LINK', '@am0708')
         temp_0708 = _emmebank.Emmebank().scenario(2007).get_attribute_values('LINK', ['auto_volume'])
@@ -47,9 +65,15 @@ def main():
     _emmebank.Emmebank().scenario(2017).set_attribute_values('LINK', ['data1'], temp_ul1)
     temp_ul2 = _emmebank.Emmebank().scenario(2016).get_attribute_values('LINK', ['auto_volume'])
     _emmebank.Emmebank().scenario(2017).set_attribute_values('LINK', ['data2'], temp_ul2)
+    
 
-    shapefile_export(_emmebank.Emmebank().scenario(2017))
-
+def main():
+    project_path = os.getcwd()
+    new_app, new_bank = new_project(project_path)
+    new_bank.open()
+    shapefile_export(project_path, new_bank.core_emmebank.scenario(2017), new_app)
+    new_bank.close()
+    # TODO: try to refactor using a dictionary for inputs. 
       
 if __name__ == '__main__':
     main()
