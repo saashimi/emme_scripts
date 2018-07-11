e3a7cd658a667ead60cc7bf3008f0a0924d93bb7
saashimi <saavedra.kevin@gmail.com>
2018-07-11 12:27:46 -0700

    add clean_emme_2_shp.py

---
+++ clean_emme_2_shp.py
@@ -0,0 +1,52 @@
+"""
+Preliminary script
+clean_emme_2_shp.py
+"""
+
+from osgeo import ogr, gdal
+import os
+
+
+def rename_fields(shp_in):
+    data_source = gdal.OpenEx(shp_in, gdal.OF_VECTOR | gdal.OF_UPDATE)
+
+    rename = {'@am0708': 'VOLS_07_08',
+              '@am0809': 'VOLS_08_09',
+              'DATA1': 'VOLS_12_13',
+              'DATA2': 'VOLS_16_17',
+              'DATA3': 'APP_CAP',
+              '@mb': 'MB_CAP'}
+
+    for key in rename:
+        data_source.ExecuteSQL('ALTER TABLE {} RENAME COLUMN {} TO {}'
+                               .format('emme_links', key, rename[key]))
+
+
+def filter_shp(shp_in):
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    data_source = driver.Open(shp_in, 0)
+    input_layer = data_source.GetLayer()
+
+    query_str = """
+                "MODES" LIKE \'%c%\'
+                AND ("VDF" IN (1, 2, 4, 9, 10))
+                AND NOT "TYPE" = 40
+                """
+
+    input_layer.SetAttributeFilter(query_str)
+
+    out_shapefile = 'out_shapefile.shp'
+    out_ds = driver.CreateDataSource(out_shapefile)
+    out_layer = out_ds.CopyLayer(input_layer, 'output_layer')
+    del input_layer, data_source, out_ds, out_layer
+
+
+def main():
+    working_dir = os.path.join(
+        os.getcwd(), "New_Project/Media/Python_exported_scenario/")
+    shp = os.path.join(working_dir, "emme_links.shp")
+    #rename_fields(shp)
+    filter_shp(shp)
+
+if __name__ == '__main__':
+    main()
