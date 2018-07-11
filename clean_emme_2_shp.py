5dbf84b2558d416a2811508cd462241c00517cbb
saashimi <saavedra.kevin@gmail.com>
2018-07-11 14:19:30 -0700

    working shapefile output script.

--- clean_emme_2_shp.py
+++ clean_emme_2_shp.py
@@ -14,6 +14,7 @@ def rename_fields(shp_in):
               '@am0809': 'VOLS_08_09',
               'DATA1': 'VOLS_12_13',
               'DATA2': 'VOLS_16_17',
+              'VOLAU': 'VOLS_17_18',
               'DATA3': 'APP_CAP',
               '@mb': 'MB_CAP'}
 
@@ -22,20 +23,20 @@ def rename_fields(shp_in):
                                .format('emme_links', key, rename[key]))
 
 
-def filter_shp(shp_in):
+def filter_shp(dir, shp_in):
     driver = ogr.GetDriverByName("ESRI Shapefile")
     data_source = driver.Open(shp_in, 0)
-    input_layer = data_source.GetLayer()
 
-    query_str = """
-                "MODES" LIKE \'%c%\'
-                AND ("VDF" IN (1, 2, 4, 9, 10))
-                AND NOT "TYPE" = 40
-                """
+    sql = """SELECT ID, INODE, JNODE, LENGTH, MODES, LANES, VDF, TYPE,
+          VOLS_07_08, VOLS_08_09, VOLS_12_13, VOLS_16_17, VOLS_17_18,
+          MB_CAP FROM {} WHERE
+          MODES LIKE '%c%'
+          AND (VDF IN (1, 2, 4, 9, 10))
+          AND NOT TYPE = 40
+          """.format('emme_links')
 
-    input_layer.SetAttributeFilter(query_str)
-
-    out_shapefile = 'out_shapefile.shp'
+    input_layer = data_source.ExecuteSQL(sql)
+    out_shapefile = os.path.join(dir, 'out_shapefile.shp')
     out_ds = driver.CreateDataSource(out_shapefile)
     out_layer = out_ds.CopyLayer(input_layer, 'output_layer')
     del input_layer, data_source, out_ds, out_layer
@@ -46,7 +47,8 @@ def main():
         os.getcwd(), "New_Project/Media/Python_exported_scenario/")
     shp = os.path.join(working_dir, "emme_links.shp")
     #rename_fields(shp)
-    filter_shp(shp)
+    filter_shp(working_dir, shp)
+
 
 if __name__ == '__main__':
     main()
--- vol_set_all_hours.py
+++ vol_set_all_hours.py
@@ -38,7 +38,7 @@ def new_project(working_dir):
         else:
             print "Path does not exist. Please enter an existing path."
             sys.exit()
-            
+
     my_app = _app.start_dedicated(False, "000", project)
     data = my_app.data_explorer()
     bank = data.add_database(emmebank_path)
@@ -67,15 +67,14 @@ def shapefile_export(working_dir, scenario, app):
 
 def attribute_copy(bank_in):
     """Aggregates volau attribute from the following times into 2017 scenario:
-        @am0708: 0700-0800
-        @am0809: 0800-0900
-        ul1:     1200-1300
-        ul2:     1600-1700
-        volau:   1700-1800
+        @am0708: 0700-0800 from Scenario 2012
+        @am0809: 0800-0900 from Scenario 2008
+        ul1:     1200-1300 from Scenario 2012
+        ul2:     1600-1700 from Scenario 2016
+        volau:   1700-1800 from Scenario 2017 (current scenario)
     """
     print 'Copying attributes...'
     updates = {'@am0708': 2007, '@am0809': 2008}
-
     for key in updates:
         if key not in bank_in.scenario(2017).attributes('LINK'):
             bank_in.scenario(2017).create_extra_attribute(
