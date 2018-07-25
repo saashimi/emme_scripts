bf3af9038f3c1df1e42189cf2a15a8b644c48f3f
saashimi <saavedra.kevin@gmail.com>
2018-07-25 12:23:43 -0700

    update docstrings.

--- create_template_folder.py
+++ create_template_folder.py
@@ -1,5 +1,13 @@
 """
 Creates Metro_Project Template folder
+by Kevin Saavedra
+
+Run this script from the directory where you would like the .emp file to
+reside.
+e.g. ../model/peak/assignPeakSpread/
+
+Useage:
+>>> python create_template_folder.py
 """
 
 import os
@@ -24,11 +32,16 @@ def copy_template_folder(working_dir):
 
 
 def path_edits(working_dir):
+    """Writes appropriate path corrections to files based on the current
+       working directory."""
     files = ['Metro_Project.emp',
              'Views/Initial.emv',
              'Views/RTP18_los.emv']
+
+    # This is the path currently found in the template files.
     old_path = 'H:/rtp/2018rtp/_round2/modelRuns/2015/iter4/model/peak/' \
                'assignPeakSpread/'
+
     project_path = os.path.join(working_dir, 'Metro_Project')
 
     for file in files:
@@ -46,6 +59,7 @@ def path_edits(working_dir):
 
 
 def main():
+    """Main program control flow."""
     project_path = os.getcwd()
     path_edits(project_path)
 
