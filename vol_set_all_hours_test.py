250f602d6c9277642ba2e478160c5880da0e904e
saashimi <saavedra.kevin@gmail.com>
2018-07-25 08:46:33 -0700

    update test file prior to test hold.

--- vol_set_all_hours_test.py
+++ vol_set_all_hours_test.py
@@ -1,14 +1,21 @@
 """
-Run this test file from a cygwin window on a server that contains the 
-INRO python API.
+Run this test file from a cygwin window on a server that contains the INRO
+python API.
 """
 
-import vol_set_all_hours
+import vol_set_all_hours as vol_set
+from vol_set_all_hours import os, shutil
 
 
-def inc(x):
-    return x + 1
+def test_new_project_does_not_exist(tmpdir):
+    """Runs function in tmpdir"""
+    emmebank = os.path.join(os.getcwd(), 'emmebank')
+    shutil.copyfile(emmebank, os.path.join(str(tmpdir), 'emmebank'))
+    vol_set.new_project(str(tmpdir))
+    assert os.path.exists('tmpdir') is True
+    assert os.path.isfile('New_Project') is True
 
 
-def test_answer():
-    assert inc(3) == 4
+def new_project_exists():
+    """test for existing project behavior."""
+    pass
