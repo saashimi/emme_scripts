daf965be862ad7b73914d72ec20227f9d12a6f16
saashimi <saavedra.kevin@gmail.com>
2018-09-13 07:54:23 -0700

    Working split and filter based on input hour.

--- 24_hr_d221_split.py
+++ 24_hr_d221_split.py
@@ -11,6 +11,22 @@ import pandas as pd
 import re
 
 
+def load_xlsx(hour_in):
+    xlsx_file = os.path.abspath(
+        'H:/rtp/2018rtp/_round2/modelRuns/2015/development/'
+        'Inputs_2015_Kate_DEV.xlsx')
+    df_transit_lines = pd.read_excel(xlsx_file, sheet_name='transit_lines')
+    df_transit_types = pd.read_excel(xlsx_file, sheet_name='transit_types')
+    df_joined = pd.merge(df_transit_lines, df_transit_types, on='TRIMET_TYPE')
+    return df_joined
+
+
+def filter_valid_transit(df_in, hour_in):
+    df_in = df_in[df_in[hour_in].notnull()]
+    transit_list = df_in['VEH_ID'].tolist()
+    return transit_list
+
+
 def clean_and_list(header_in):
     """Converts raw text lines to cleaned lists.
     Input: header_in, the header line.
@@ -21,7 +37,6 @@ def clean_and_list(header_in):
     stripped_name = ' '.join(list_match[1].split())
     header_in = header_in.replace(list_match[0], stripped_id)
     header_in = header_in.replace(list_match[1], stripped_name)
-
     return filter(None, header_in.rstrip().split(' '))
 
 
@@ -36,39 +51,38 @@ def header_parser(list_in):
     return list_in
     
 
-def main(network_file):
+def main(network_file, hour):
     """
     Main network parse script.
     inputs: network_file,  a d221 network file supplied as user argument.
     """
-    xlsx_file = os.path.abspath(
-        'H:/rtp/2018rtp/_round2/modelRuns/2015/development/'
-        'Inputs_2015_Kate_DEV.xlsx')
-    df_transit_lines = pd.read_excel(xlsx_file, sheet_name='transit_lines')
-    df_transit_types = pd.read_excel(xlsx_file, sheet_name='transit_types')
-
-    df_joined = pd.merge(df_transit_lines, df_transit_types, on='TRIMET_TYPE')
-   
-
+    df = load_xlsx(hour)
+    filtered_transit = filter_valid_transit(df, hour)
+    print filtered_transit
+    
     with open(network_file, 'r') as src:
         write_line_marker = False
 
         for line in src:
+                        
             if "a'" in line:
+                if any(transit in line for transit in filtered_transit):
 
-                filename = line[:8].replace(' ', '')
-                filename = filename.replace("'", '')
-                filename = filename[1:] + '.txt'
+                    filename = line[:8].replace(' ', '')
+                    filename = filename.replace("'", '')
+                    filename = filename[1:] + '.txt'
 
-                header_list = clean_and_list(line)
-                edited_header = header_parser(header_list)
+                    header_list = clean_and_list(line)
+                    edited_header = header_parser(header_list)
 
-                with open(filename, 'w') as dest:
-                    str = ''
-                    for item in edited_header:
-                        str = str + item + '  '
-                    dest.write(str + '\n')
-                    write_line_marker = True
+                    with open(filename, 'w') as dest:
+                        str = ''
+                        for item in edited_header:
+                            str = str + item + '  '
+                        dest.write(str + '\n')
+                        write_line_marker = True
+                else:
+                    write_line_marker = False
 
             elif write_line_marker:
                 with open(filename, 'a') as dest:
@@ -76,4 +90,4 @@ def main(network_file):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
